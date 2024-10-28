2660d402f03114dc3daf6c37d4a27de74ad33372
Tim Hrdy <tim.hrdy.1@gmail.com>
2024-10-28 16:04:00 +0100

    V1.2 bef merge

--- users/jest.config.js
+++ users/jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
-  testTimeout: 30000, // Augmenter le délai d'attente à 30 secondes
-  setupFilesAfterEnv: ["<rootDir>/src/config/jest.setup.ts"], // Ajouter cette ligne pour inclure jest.setup.ts
+  setupFilesAfterEnv: ["<rootDir>/src/config/jest.setup.ts"],
 };
