4890d75969032569fa3c9b008fbd8b84b8fc1419
Tim Hrdy <tim.hrdy.1@gmail.com>
2024-11-07 09:19:26 +0100

    v2

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -1,78 +1,28 @@
-// gateway.js
+// app.js
 import express, { json } from "express";
-import axios from "axios";
+import cors from "cors";
+import setupSwagger from "./config/Swagger.js";
+
+import {
+  handleUsersRequest,
+  handleOrdersRequest,
+  handleProductsRequest,
+  handleCartRequest,
+} from "./controllers/ApiGatewayController.js";
 
 const app = express();
-const PORT = 3005;
+const PORT = process.env.PORTGATEWAY || 3005;
 
-// Middleware pour parser le corps des requêtes
-app.use(json());
+// Middleware pour parser le JSON
+app.use(cors());
+app.use(express.json());
 
-// Route vers le microservice des utilisateurs
-app.use("/order", async (req, res) => {
-  try {
-    const response = await axios({
-      method: req.method,
-      url: `http://localhost:3000${req.originalUrl}`, // URL du microservice des orders
-      data: req.body,
-    });
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    res.status(error.response ? error.response.status : 500).json({
-      message: error.message,
-    });
-  }
-});
+// Routes vers les microservices
+app.use("/users", handleUsersRequest);
+app.use("/orders", handleOrdersRequest);
+app.use("/products", handleProductsRequest);
+app.use("/cart", handleCartRequest);
 
-// Route vers le microservice des utilisateurs
-app.use("/users", async (req, res) => {
-  try {
-    const response = await axios({
-      method: req.method,
-      url: `http://localhost:3001${req.originalUrl}`, // URL du microservice des utilisateurs
-      data: req.body,
-    });
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    res.status(error.response ? error.response.status : 500).json({
-      message: error.message,
-    });
-  }
-});
-
-// Route vers le microservice des produits
-app.use("/products", async (req, res) => {
-  try {
-    const response = await axios({
-      method: req.method,
-      url: `http://localhost:3002${req.originalUrl}`, // URL du microservice des produits
-      data: req.body,
-    });
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    res.status(error.response ? error.response.status : 500).json({
-      message: error.message,
-    });
-  }
-});
-
-// Route vers le microservice des carts
-app.use("/cart", async (req, res) => {
-  try {
-    const response = await axios({
-      method: req.method,
-      url: `http://localhost:3003${req.originalUrl}`, // URL du microservice des utilisateurs
-      data: req.body,
-    });
-    res.status(response.status).json(response.data);
-  } catch (error) {
-    res.status(error.response ? error.response.status : 500).json({
-      message: error.message,
-    });
-  }
-});
-
-// Lancer l'API Gateway
 app.listen(PORT, () => {
   console.log(`API Gateway running on port ${PORT}`);
 });
